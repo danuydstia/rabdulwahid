c399bd3a7f425576b353e9c4d02505f607910b1b
Cefroad <149056515+Cefroad@users.noreply.github.com>
2026-04-26 17:46:06 +0700

    Updates

--- _next/static/D70l9zHhWNRiYzu8ho4CW/_buildManifest.js
+++ _next/static/D70l9zHhWNRiYzu8ho4CW/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/rabdulwahid//_next/:path+",
-        "destination": "/rabdulwahid/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
